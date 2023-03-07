ef5853f8329ab53f9254867357521ec1c58d38de
Vincentius Willy Ardiyanto <72459873+WillyArdiyanto@users.noreply.github.com>
2023-03-07 10:07:29 +0700

    update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Halo from Cloud Run!');
+	res.send('Willy-desu from Cloud Run!');
 });
 
 const PORT = process.env.PORT || 8080;
