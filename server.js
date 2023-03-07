3f1c974a132905be289f7051aa2a3cbc8ca8d643
Shazi Awaludin <72121184+Xharf@users.noreply.github.com>
2023-03-07 14:53:46 +0700

    create something

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Hello from Cloud Run!');
+	res.send('pffttt from Cloud Run!');
 });
 
 const PORT = process.env.PORT || 8080;
