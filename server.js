ed423f6b5202d9d495304e3555ba86351193528f
Vincentius Willy Ardiyanto <72459873+WillyArdiyanto@users.noreply.github.com>
2023-03-10 17:16:21 +0700

    Change res.send

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Im shazi!');
+	res.send('Im Vano!');
 });
 
 const PORT = process.env.PORT || 8080;
